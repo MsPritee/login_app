b0324c9f1a154442343621546270e52a246249a3
MsPritee <badgujarpriti5@gmail.com>
2023-10-13 21:57:53 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/login_app" element={<Login />} /> 
+          <Route path="/" element={<Login />} /> 
           <Route path="/registration" element={<Registration />} /> 
         </Routes>
       </div>
--- src/Components/Registration.js
+++ src/Components/Registration.js
@@ -9,7 +9,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export const Registration = () => {
     return (
         <>
-            
+
             <div className='main relative min-h-screen'>
                 <header className='bg-blue-600  '>
                     <div className=' left-0  flex items-center h-fit justify-center md:justify-start md:ml-24'>
@@ -44,88 +44,95 @@ export const Registration = () => {
                         <h2 className=' text-xl font-bold p-3  lg:text-xl  border-b '>
                             Recruiter Registration
                         </h2>
-
-                        <div className='registration-form font-medium flex gap-2 :gap-6  mx-1 rounded '>
-                            <div className='left-side-box  flex flex-auto flex-col m-1'>
-                                <div className='name-box  flex gap-1 my-5'>
-                                    <div className='name_title  '>
-                                        <label htmlFor="country">Saluation</label>
-                                        <div id='name-title' className='name-title pr-1  ' >
-                                            <select className='name-title pl-2 border rounded mr-1 h-10' id="title" name="title" >
-                                                <option value="">-Select-</option>
-                                                <option value="Mrs.">Mrs.</option>
-                                                <option value="Miss">Miss.</option>
-                                                <option value="Ms">Ms.</option>
-                                            </select>
+                        <div >
+                            <div className='registration-form font-medium flex gap-2 :gap-6  mx-1 rounded '>
+                                <div className='left-side-box  flex flex-auto flex-col m-1'>
+                                    <div className='name-box  flex gap-1 my-5'>
+                                        <div className='name_title  '>
+                                            <label htmlFor='saluation'>Saluation</label>
+                                            <div id='name-saluation' className='name-title pr-1  ' >
+                                                <select className=' saluation pl-2 border rounded mr-1 w-full' id="saluation" name="title" >
+                                                    <option value="">-Select-</option>
+                                                    <option value="Mrs.">Mrs.</option>
+                                                    <option value="Miss">Miss.</option>
+                                                    <option value="Mr">Mr.</option>
+                                                    <option value="Dr">Dr.</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className='name flex flex-col flex-1 '>
+                                            <label >Name</label>
+                                            <input id="name" className='name-input flex-1 w-full ' type='text' placeholder='Enter Full Name' />
                                         </div>
                                     </div>
-                                    <div className='name-box flex flex-col flex-1 '>
-                                        <label >Name</label>
-                                        <input id="name" className='name-input flex-1 w-full ' type='text' placeholder='Name' />
+                                    <div className='company-email mb-5  flex flex-col'>
+                                        <label >Company Email ID</label>
+                                        <input className='company-input' type='text' placeholder='Enter Company Email ID' />
                                     </div>
-                                </div>
-                                <div className='company-email mb-5  flex flex-col'>
-                                    <label >Company Email ID</label>
-                                    <input className='company-input' type='text' placeholder='Company' />
-                                </div>
 
-                                <div className='password  '>
-                                    <label className='mb-2'> Password </label>
-                                    <div className='Pass-container relative '>
-                                        <input className='password-input ' type='password' placeholder='Password' />
-                                        <span className="eye h-5 w-5 cursor-pointer absolute right-2 top-1/2 transform -translate-y-1/2" >
-                                            <img src={Eye} alt="Toggle Password" />
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div className='cancel-btn w-full'>
-                                    <button className='bg-gray-100 mt-8  py-2.5 px-4 rounded hover:bg-slate-300 hover:text-black hover:font-bold '>
-                                        Cancel
-                                    </button>
-                                </div>
-
-                            </div>
-
-                            <div className='right-side-box flex-auto flex flex-col m-1'>
-                                <div className='company-detail justify-between flex gap-4 my-5'>
-                                    <div className='company-name flex-auto flex flex-col' >
-                                        <label >Company Name</label>
-                                        <input id='company-input' className='  company-name' type='text' placeholder='Enter Company Name' />
-                                    </div>
-                                    <div className='designation flex-auto flex flex-col'>
-                                        <label >Designation</label>
-                                        <input id='company-input' className='' type='text' placeholder='Enter Designation' />
-                                    </div>
-                                </div>
-                                <div className='personal-email  flex flex-col'>
-                                    <label >Personal Email ID</label>
-                                    <input className='i' type='text' placeholder='Enter Personal Email ID' />
-                                </div>
-                                <div className='personal-detail flex gap-1 my-5 w-full'>
-                                    <div className='county-select'>
-                                        <label htmlFor="country">Country</label>
-                                        <div id="select-country" className='name-title pr-1'>
-                                            <select id="country" name="country pl-2">
-                                                <option value="">Select</option>
-                                                <option value="usa">USA</option>
-                                                <option value="canada">India</option>
-                                                <option value="canada">Canada</option>
-                                            </select>
+                                    <div className='password  '>
+                                        <label className='mb-2'> Password </label>
+                                        <div className='Pass-container relative '>
+                                            <input className='password-input ' type='password' placeholder='Enter Password' />
+                                            <span className="eye h-5 w-5 cursor-pointer absolute right-2 top-1/2 transform -translate-y-1/2" >
+                                                <img src={Eye} alt="Toggle Password" />
+                                            </span>
                                         </div>
                                     </div>
-                                    <div id="mobile" className='flex flex-col flex-auto'>
-                                        <label htmlFor="mobileNumber">Mobile Number</label>
-                                        <input type="tel" id="mobileNumber" className='w-full' name="mobileNumber" />
-                                    </div>
+
                                 </div>
 
-                                <div className='continue-btn  w-full  '>
-                                    <button className=' bg-blue-800 text-white mt-3 py-2.5 px-4 rounded hover:bg-slate-300 hover:text-black hover:font-bold '>
-                                        Continue
-                                    </button>
+                                <div className='right-side-box flex-auto flex flex-col m-1'>
+                                    <div className='company-detail justify-between flex gap-4 my-5'>
+                                        <div className='company-name flex-auto flex flex-col' >
+                                            <label >Company Name</label>
+                                            <input id='company-input' className='  company-name' type='text' placeholder='Enter Company Name' />
+                                        </div>
+                                        <div className='designation flex-auto flex flex-col'>
+                                            <label >Designation</label>
+                                            <input id='company-input' className='' type='text' placeholder='Enter Designation' />
+                                        </div>
+                                    </div>
+                                    <div className='personal-email  flex flex-col'>
+                                        <label >Personal Email ID</label>
+                                        <input className='i' type='text' placeholder='Enter Personal Email ID' />
+                                    </div>
+                                    <div className='personal-detail flex gap-1 my-5 w-full'>
+                                        <div className='county-select'>
+                                            <label htmlFor="country">Country</label>
+                                            <div id="select-country" className='name-title '>
+                                                <select id="country" name="country pl-2">
+                                                    <option value="">Select</option>
+                                                    <option value="usa">USA</option>
+                                                    <option value="canada">India</option>
+                                                    <option value="canada">Canada</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div id="mobile" className='flex flex-col flex-auto'>
+                                            <label htmlFor="mobileNumber">Mobile Number</label>
+                                            <input type="tel" id="mobileNumber" placeholder='Enter Mobile Number' className='w-full' name="mobileNumber" />
+                                        </div>
+                                    </div>
+
+
                                 </div>
                             </div>
+                            <div className='flex flex-row w-full justify-between px-3 pb-2'>
+                                    {/* <div className='cancel-btn w-full'> */}
+                                        <button className='bg-gray-100 border mt-3  py-1.5 px-4 rounded hover:bg-slate-300 hover:text-black hover:font-bold '>
+                                            Cancel
+                                        </button>
+                                    {/* </div> */}
+
+                                    {/* <div className='continue-btn  w-full  '> */}
+                                        <button className=' bg-blue-800 right-0 text-white mt-3 py-1.5 px-4 rounded hover:bg-slate-300 hover:text-black hover:font-bold '>
+                                            Continue
+                                        </button>
+                                    {/* </div> */}
+                                </div>
+
+                           
                         </div>
                     </div>
                 </div>
